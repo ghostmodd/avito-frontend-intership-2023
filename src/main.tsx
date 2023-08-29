aaf7d6f056e94d9cf9cb3630b7ec2079a0ec42ff
ghostmodd <ghost1mod@gmail.com>
2023-08-29 23:51:26 +1000

    chore: set project structure

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { store } from "./app/store"
-import App from "./App"
-import "./index.css"
+import App from "./components/App/App"
+import {BrowserRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
+    <Provider store={ store }>
+      <BrowserRouter>
+        <App/>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
 )
