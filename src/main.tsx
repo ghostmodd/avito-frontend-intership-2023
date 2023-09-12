8b61d5ae6cf87fedeef5bbc5bb6b2045b8723c49
ghostmodd <ghost1mod@gmail.com>
2023-09-12 23:44:36 +0300

    fix: correct index.html

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,16 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./components/App/App"
-import { BrowserRouter } from "react-router-dom"
+import {HashRouter} from "react-router-dom"
 import { Provider } from "react-redux"
 import { store } from "./store/store"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
