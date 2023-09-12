51453dda3f69a7bfb306458f4aba275434d5c373
ghostmodd <ghost1mod@gmail.com>
2023-09-12 22:39:35 +0300

    feature: add sh scripts for deploying project

---
+++ deploy.sh
@@ -0,0 +1,47 @@
+bash
+
+# Установка параметров
+BUILD_BRANCH="build"
+DEVELOP_BRANCH="develop"
+REPO_URL="https://github.com/ghostmodd/avito-frontend-intership-2023.git"
+
+
+# Создание новой сборки
+npm run build
+
+# Создание временной директории
+mkdir build_repo
+
+# Переход ко временной директории
+cd build_repo
+
+# Копирование данных из build во временную директорию
+
+
+# Переход в ветку build
+git checkout $BUILD_BRANCH
+
+# Клонирование репозитория в отдельную директорию
+git clone $REPO_URL build_repo
+cd build_repo
+
+# Переключение на ветку "develop"
+git checkout $DEVELOP_BRANCH
+
+# Копирование новой сборки в репозиторий
+cp -R ../build/* .
+
+# Добавление изменений в индекс
+git add .
+
+# Создание коммита
+git commit -m "Update build"
+
+# Отправка изменений на ветку "develop"
+git push origin $DEVELOP_BRANCH
+
+# Возврат к директории проекта
+cd ..
+
+# Удаление временной директории репозитория
+rm -rf build_repo
